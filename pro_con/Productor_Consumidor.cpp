d7568901870f053dbacad8a9249c64915f8075e1
UzielMo4-18 <uziel.moreno@iberopuebla.mx>
2021-10-21 12:49:44 -0500

    Actualización: Prioridad para productores

--- pro_con/Productor_Consumidor.cpp
+++ pro_con/Productor_Consumidor.cpp
@@ -6,42 +6,68 @@
 #include <time.h>
 #include <semaphore.h>
 
-#define THREAD_NUM 2
+#define THREAD_NUM 5
 
 sem_t semEmpty;
 sem_t semFull;
 
 pthread_mutex_t mutexBuffer;
+pthread_mutex_t mutexA;
 
 int buffer[10];
-int count = 0;
+int count = 0, nc = 0, npe = 0;
+bool escribiendo = false;
 
 void* producer(void* args){
     while(1){
         int x = rand() % 100;
         sleep(1);
-
-        sem_wait(&semEmpty);
-        pthread_mutex_lock(&mutexBuffer);
+        pthread_mutex_lock(&mutexA);
+        npe++;
+        printf("Numero de productores en espera->%d",npe);
+        while (nc > 0 || escribiendo)
+        {
+            pthread_mutex_unlock(&mutexA);
+            pthread_mutex_lock(&mutexA);
+        }
+        
+        //sem_wait(&semEmpty);
+        escribiendo = true;
+        npe--;
+        pthread_mutex_unlock(&mutexA);
+        //pthread_mutex_lock(&mutexBuffer);
         buffer[count] = x;
         count++;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semFull);
+        pthread_mutex_lock(&mutexA);
+        //pthread_mutex_unlock(&mutexBuffer);
+        escribiendo = false;
+        pthread_mutex_unlock(&mutexA);
+        //sem_post(&semFull);
     }
 }
 
 void* consumer(void* args){
     while(1){
         int y;
-
-        sem_wait(&semFull);
-        pthread_mutex_lock(&mutexBuffer);
+        pthread_mutex_lock(&mutexA);
+        //sem_wait(&semFull);
+        while (npe > 0 || escribiendo)
+        {
+            pthread_mutex_unlock(&mutexA);
+            pthread_mutex_lock(&mutexA);
+        }
+        
+        nc++;
+        printf("\nNumero de consumidores ->%d",nc);
+        pthread_mutex_unlock(&mutexA);
+        //pthread_mutex_lock(&mutexBuffer);
         y = buffer[count - 1];
         count--;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semEmpty);
+        //pthread_mutex_unlock(&mutexBuffer);
+        nc--;
+        //sem_post(&semEmpty);
 
-        printf("Obtuvo %d\n", y);
+        printf("\nObtuvo %d\n", y);
         sleep(1);
     }
 }
@@ -50,11 +76,12 @@ int main(int argc, char* argv[]){
     srand(time(NULL));
     pthread_t th[THREAD_NUM];
     pthread_mutex_init(&mutexBuffer, NULL);
+    pthread_mutex_init(&mutexA, NULL);
     sem_init(&semEmpty, 0, 10);
     sem_init(&semFull, 0, 0);
     int i;
     for (i = 0 ;i < THREAD_NUM; i++){
-        if(i % 2 == 0){
+        if(i % 2 != 0){
             if(pthread_create(&th[i], NULL, &producer, NULL) != 0) perror("Creación de hilo fallida");
         }
         else{
@@ -67,5 +94,6 @@ int main(int argc, char* argv[]){
     sem_destroy(&semEmpty);
     sem_destroy(&semFull);
     pthread_mutex_destroy(&mutexBuffer);
+    pthread_mutex_destroy(&mutexA);
     return 0;
 }
